d969dc893f7ccbd91cea0455df0746d5bbbdae66
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-28 17:56:28 +0100

    Add React & ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log([1, 2, 3].map(num => num ** 2));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => <h1>React with Webpack and Babel</h1>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
