4cd1bf199a21fae2c842a3f8f4c752219d3e1f67
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-28 18:42:12 +0100

    Add example stateful component & fetch remote data using hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,62 @@
-import React from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
+import axios from 'axios';
 
-const App = ({ title }) => <h1>{title}</h1>;
+const dataReducer = (state, action) => {
+  if (action.type === 'SET_ERROR') {
+    return { ...state, list: [], error: true };
+  }
+  if (action.type === 'SET_LIST') {
+    return { ...state, list: action.list, error: null };
+  }
+  throw new Error();
+};
+
+const initialData = {
+  list: [],
+  error: null,
+};
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [data, dispatch] = useReducer(dataReducer, initialData);
+
+  useEffect(() => {
+    axios
+      .get('http://hn.algolia.com/api/v1/search?query=react')
+      .then(response => {
+        dispatch({ type: 'SET_LIST', list: response.data.hits });
+      })
+      .catch(() => {
+        dispatch({ type: 'SET_ERROR' });
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Counter</h1>
+      <Counter counter={counter} />
+      <button type="button" onClick={() => setCounter(counter + 1)}>
+        Increment
+      </button>
+      <button type="button" onClick={() => setCounter(counter - 1)}>
+        Decrement
+      </button>
+
+      <h2>Async Data</h2>
+      {data.error && <div className="error">Error</div>}
+      <ul>
+        {data.list.map(item => (
+          <li key={item.objectID}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export const Counter = ({ counter }) => (
+  <div>
+    <p>{counter}</p>
+  </div>
+);
 
 export default App;
