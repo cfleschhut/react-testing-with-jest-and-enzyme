50b900b85b5ebe00c7454c89ec8c34aac6b831c3
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-28 20:53:43 +0100

    Add snapshot tests

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,5 +1,21 @@
-describe('Test Suite', () => {
-  it('Test Case', () => {
-    expect(true).toEqual(true);
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App, { Counter } from './App';
+
+describe('App', () => {
+  test('snapshot renders', () => {
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Counter', () => {
+  test('snapshot renders', () => {
+    const component = renderer.create(<Counter counter={1} />);
+    let tree = component.toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 });
