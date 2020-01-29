e835d42190b0fff84eae95834f455d591bf930b0
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-01-29 11:20:10 +0100

    Test rendering of Counter child component inside App parent component

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 import App, { Counter, dataReducer } from './App';
 
 const list = ['a', 'b', 'c'];
@@ -33,6 +34,19 @@ describe('App', () => {
     const tree = component.toJSON();
 
     expect(tree).toMatchSnapshot();
+  });
+
+  it('renders the inner Counter', () => {
+    const wrapper = mount(<App />);
+
+    expect(wrapper.find(Counter).length).toEqual(1);
+  });
+
+  it('passes all props to Counter', () => {
+    const wrapper = mount(<App />);
+    const counterWrapper = wrapper.find(Counter);
+
+    expect(counterWrapper.find('p').text()).toEqual('0');
   });
 });
 
